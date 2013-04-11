2cbf58aaf16649d176e3c34c1c38b116e4dc760b
zendawg <rich@statefive.org>
2013-04-11 12:30:38 +0100

    Drop-down menu.

--- protected/views/base/_form.php
+++ protected/views/base/_form.php
@@ -16,48 +16,51 @@
  * @copyright Copyright (c) 2011-2012, OpenEyes Foundation
  * @license http://www.gnu.org/licenses/gpl-3.0.html The GNU General Public License V3.0
  */
-$uri = preg_replace('/\/$/','',$_SERVER['REQUEST_URI']);
+$uri = preg_replace('/\/$/', '', $_SERVER['REQUEST_URI']);
 ?>
 <?php
 if (!Yii::app()->user->isGuest) {
-	if (!empty(Yii::app()->session['user'])) {
-		$user = Yii::app()->session['user'];
-	} else {
-		$user = User::model()->findByPk(Yii::app()->user->id);
-		Yii::app()->session['user'] = $user;
-	} ?>
-	<div id="user_panel">
-		<div class="clearfix">
-			<div id="user_id">
-				Hi <strong><?php echo $user->first_name . ' ' . $user->last_name; ?></strong>&nbsp;<!--a href="#" class="small">(not you?)</a-->
-			</div>
+    if (!empty(Yii::app()->session['user'])) {
+        $user = Yii::app()->session['user'];
+    } else {
+        $user = User::model()->findByPk(Yii::app()->user->id);
+        Yii::app()->session['user'] = $user;
+    }
+    ?>
+    <div id="user_panel">
+        <div class="clearfix">
+            <div id="user_id">
+                Hi <strong><?php echo $user->first_name . ' ' . $user->last_name; ?></strong>&nbsp;<!--a href="#" class="small">(not you?)</a-->
+            </div>
 
-			<ul id="user_nav">
-				<li><?php if ($uri == Yii::app()->baseUrl) { ?><span class="selected">Home</span><?php } else { ?><?php echo CHtml::link('Home',Yii::app()->baseUrl.'/')?><?php } ?></li>
-				<?php if(BaseController::checkUserLevel(2)) { ?>
-				<li><?php if ($uri == Yii::app()->baseUrl.'/theatre') { ?><span class="selected">Theatre Diaries</span><?php } else { ?><?php echo CHtml::link('Theatre Diaries',array(Yii::app()->baseUrl.'/theatre/'))?><?php } ?></li>
-				<li><?php if ($uri == Yii::app()->baseUrl.'/waitingList') { ?><span class="selected">Partial bookings waiting List</span><?php } else { ?><?php echo CHtml::link('Partial bookings waiting list',array(Yii::app()->baseUrl.'/waitingList/'))?><?php } ?></li>
-				<?php } ?>
-				<li><?php echo CHtml::link('Logout', array(Yii::app()->baseUrl.'/site/logout')) ?></li>
-			</ul>
-		</div>
-		<?php
-		if ($this->showForm) {?>
-			<div id="user_firm">
-				<?php
-				$form=$this->beginWidget('CActiveForm', array(
-					'id'=>'base-form',
-					'enableAjaxValidation'=>false,
-					'action' => Yii::app()->createUrl('site')
-				));
-				?>
-				<label>Selected firm: </label>
-				<?php
-				echo CHtml::dropDownList('selected_firm_id', $this->selectedFirmId, $this->firms);
-				$this->endWidget();
-				?>
-			</div>
-		<?php }?>
-	</div> <!-- #user_panel -->
-<?php
-}?>
+            <ul id="user_nav">
+
+                <select id="urlList" onchange="window.location.href = this.value">
+                    <option>Choose...</option>
+                    <option value="/virtualClinic/results/0/0/0/0/0">Virtual Clinic</option>
+                    <option value="/">Search</option>
+                </select>
+
+
+                <li><?php echo CHtml::link('Logout', array(Yii::app()->baseUrl . '/site/logout')) ?></li>
+            </ul>
+        </div>
+        <?php if ($this->showForm) { ?>
+            <div id="user_firm">
+                <?php
+                $form = $this->beginWidget('CActiveForm', array(
+                    'id' => 'base-form',
+                    'enableAjaxValidation' => false,
+                    'action' => Yii::app()->createUrl('site')
+                        ));
+                ?>
+                <label>Selected firm: </label>
+                <?php
+                echo CHtml::dropDownList('selected_firm_id', $this->selectedFirmId, $this->firms);
+                $this->endWidget();
+                ?>
+            </div>
+        <?php } ?>
+    </div> <!-- #user_panel -->
+    <?php }
+?>
