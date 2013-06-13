bf6e38c7149f3974c7e58deaa9f9dee373b36d16
zendawg <rich@statefive.org>
2013-06-13 14:48:12 +0100

    Segregated ESB API from main REST controller; added custom esb properties.

---
+++ protected/controllers/EsbRestApiController.php
@@ -0,0 +1,174 @@
+<?php
+
+/**
+ * Taken and adapted from:
+ *  http://www.yiiframework.com/wiki/175/how-to-create-a-rest-api/
+ */
+class EsbRestApiController extends RestfulController {
+  
+  function getViewableModels() {
+    return array_merge($this->getCreatableModels(), 
+            Yii::app()->params['esb_rest_api_viewable']);
+  }
+  function getCreatableModels() {
+    return $this->getUpdatableModels();
+  }
+  function getUpdatableModels() {
+    return Yii::app()->params['esb_rest_api_updatable'];
+  }
+  function getSearchableModels() {
+    return $this->getViewableModels();
+  }
+  
+  public function getApplicationId() {
+    return Yii::app()->params['esb_rest_api_id'];
+  }
+  
+  function actionRest() {
+    return parent::actionRest();
+  }
+  
+  /**
+   * 
+   */
+  function actionCreateHumphreyImagePairEvent() {
+    $this->_checkAuth();
+    if (!isset($_GET['patient_id'])) {
+      $this->_sendResponse(500, 'Error: Parameter pid is missing');
+    }
+    if (!isset($_GET['tif_file_id'])) {
+      $this->_sendResponse(500, 'Error: Parameter tif_file_id is missing');
+    }
+    if (!isset($_GET['xml_id'])) {
+      $this->_sendResponse(500, 'Error: Parameter xml_id is missing');
+    }
+    if (!isset($_GET['test_strategy'])) {
+      $this->_sendResponse(500, 'Error: Parameter test_Strategy is missing');
+    }
+    $pid = $_GET['patient_id'];
+    $tif_file_id = $_GET['tif_file_id'];
+    $xml_id = $_GET['xml_id'];
+    $test_strategy = $_GET['test_strategy'];
+    
+    $event_type = EventType::model()->find('class_name=\'OphInVisualfields\'');
+    $patient = Patient::model()->find('hos_num=' . $pid);
+    if ($patient && $event_type) {
+      
+      $episodes = Episode::model()->findAll('patient_id=' . $patient->id);
+      if (count($episodes) == 1) {
+        
+          $xml_image = FsScanHumphreyXml::model()->find('id=' . $xml_id);
+
+          $createdDate = new DateTime($xml_image->study_date);
+          $createdTime = new DateTime($xml_image->study_date . ' ' . $xml_image->study_time);
+          $x = $createdTime->sub(new DateInterval('PT1H2M'))->format('H:i:s');
+          $preTime = $createdTime->sub(new DateInterval('PT1H2M'));
+          $t = $preTime->format('H:i:s');
+          $y = $createdDate->format('Y-m-d');
+          $eye = 'L';
+          if ($xml_image->eye == 'L') {
+            $eye = 'R';
+          }
+          $images = FsScanHumphreyXml::model()->findAll(
+                  'pid=\'' . $patient->hos_num
+                  . '\' and study_date>=\'' . $createdDate->format('Y-m-d')
+                  . '\' and study_time>=\'' . $preTime->format('H:i:s')
+                  . '\' and associated=0 and eye=\'' . $eye . '\''
+                  );
+          if (count($images) > 0) {
+            $tifCriteria = new CDbCriteria;
+            $tifCriteria->addCondition('file_id=\'' . $images[0]->tif_file_id . '\'');
+            $previous_tif = FsScanHumphreyImage::model()->find($tifCriteria);
+            // the current tif that is being tested for:
+            $tifCriteria = new CDbCriteria;
+            $tifCriteria->addCondition('file_id=\'' . $tif_file_id . '\'');
+            $tif_image = FsScanHumphreyImage::model()->find($tifCriteria);
+            if ($images[0]->eye == 'R') {
+              $tmp = $previous_tif->file_id;
+              $previous_tif->file_id = $tif_image->file_id;
+              $tif_image->file_id = $tmp;
+            }
+            
+            $testType = OphInVisualfields_Testtype::model()->find('name=\'Humphreys\'');
+            $testStrategy = OphInVisualfields_Strategy::model()->find('name=\'' . $test_strategy . '\'');
+
+            $event = new Event;
+            $event->episode_id = $episodes[0]->id;
+            $event->event_type_id = $event_type->id;
+            
+            $username = $_SERVER['HTTP_X_' . $this->getApplicationId() . '_USERNAME'];
+            $event->created_user_id = User::model()->find('username=\'' . $username. '\'')->id;
+            $event->last_modified_user_id = User::model()->find('username=\'' . $username. '\'')->id;
+            $event->created_date = date($xml_image->study_date
+                    . ' ' . $xml_image->study_time);
+            $event->last_modified_date = date($xml_image->study_date
+                    . ' ' . $xml_image->study_time);
+            $t = $event->created_date;
+            $event->datetime= date($xml_image->study_date
+                    . ' ' . $xml_image->study_time);
+            $event->save($allow_overriding=true);
+            
+            $event->created_user_id = User::model()->find('username=\'' . $username. '\'')->id;
+            $event->last_modified_user_id = User::model()->find('username=\'' . $username. '\'')->id;
+            $event->created_date = date($xml_image->study_date
+                    . ' ' . $xml_image->study_time);
+            $t = $event->created_date;
+            $event->datetime= date($xml_image->study_date
+                    . ' ' . $xml_image->study_time);
+            $event->last_modified_date = date($xml_image->study_date
+                    . ' ' . $xml_image->study_time);
+            $event->save($allow_overriding=true);
+            
+            $objTestType = new Element_OphInVisualfields_Testtype;
+            $objTestType->event_id = $event->id;
+            $objTestType->test_type_id = $testType->id;
+            $objTestType->save();
+            $objDetails = new Element_OphInVisualfields_Details;
+            $objDetails->event_id = $event->id;
+            $objDetails->strategy_id = $testStrategy->id;
+            $objDetails->save();
+            $objImage = new Element_OphInVisualfields_Image;
+            $objImage->event_id = $event->id;
+            $objImage->left_image = $previous_tif->file_id;
+            $objImage->right_image = $tif_image->file_id;
+            $objImage->save();
+            
+            $image1 = FsScanHumphreyXml::model()->find('id=' . $xml_id);
+            $image1->associated = 1;
+            $image1->save();
+            $image2 = FsScanHumphreyXml::model()->find('id=' . $images[0]->id);
+            $image2->associated = 1;
+            $image2->save();
+          }
+      }
+    }
+    
+  }
+  
+//  public function actionDelete() {
+//    $this->_checkAuth();
+//    switch ($_GET['model']) {
+//      // Load the respective model
+//      case 'asset':
+//        $model = Asset::model()->findByPk($_GET['id']);
+//        break;
+//      default:
+//        $this->_sendResponse(501, sprintf('Error: Mode delete is not implemented for model \'%s\'', $_GET['model']));
+//        Yii::app()->end();
+//    }
+//    // Was a model found? If not, raise an error
+//    if ($model === null)
+//      $this->_sendResponse(400, sprintf("Error: Didn't find any model \'%s\' with ID \'%s\'.", $_GET['model'], $_GET['id']));
+//
+//    // Delete the model
+//    $num = $model->delete();
+//    if ($num > 0)
+//      $this->_sendResponse(200, $num);    //this is the only way to work with backbone
+//    else
+//      $this->_sendResponse(500, sprintf("Error: Couldn't delete model \'%s\' with ID \'%s\'.", $_GET['model'], $_GET['id']));
+//  }
+
+
+}
+
+?>
--- protected/controllers/RestfulController.php
+++ protected/controllers/RestfulController.php
@@ -4,12 +4,7 @@
  * Taken and adapted from:
  *  http://www.yiiframework.com/wiki/175/how-to-create-a-rest-api/
  */
-class RestfulController extends Controller {
-  
-  /**
-   * Key which has to be in HTTP USERNAME and PASSWORD headers 
-   */
-  Const APPLICATION_ID = 'ASCCPE';
+abstract class RestfulController extends Controller {
 
   /**
    * @return array action filters
@@ -17,17 +12,35 @@ class RestfulController extends Controller {
   public function filters() {
     return array();
   }
+  
+  /**
+   * 
+   */
+  abstract function getViewableModels();
+  /**
+   * 
+   */
+  abstract function getCreatableModels();
+  /**
+   * 
+   */
+  abstract function getUpdatableModels();
+  /**
+   * 
+   */
+  abstract function getSearchableModels();
 
+  
+  /**
+   * Key which has to be in HTTP USERNAME and PASSWORD headers 
+   */
+  abstract function getApplicationId();
+  
   /**
    * Perform a get with ID (view), get without ID (list), create (post) or
    * update (put) with ID.
    */
-  public function actionRest() { 
-    // until this has been fully adapted and modifed, localhost access only
-    // TODO remove when API is more mature 
-    if (Yii::app()->request->getBaseUrl(true) != 'http://localhost') {
-      return;
-    }
+  public function actionRest() {
     $this->_checkAuth();
     $requestMethod = $_SERVER['REQUEST_METHOD'];
     switch ($requestMethod) {
@@ -73,6 +86,9 @@ class RestfulController extends Controller {
    * @param type $model
    */
   private function viewAll($model) {
+    if (!in_array($model, $this->getViewableModels())) {
+      $this->_sendResponse(500, 'Error: REST transation are not supported for ' . $model);
+    }
     // Get the respective model instance
     try {
       $class_name = new $model;
@@ -102,6 +118,9 @@ class RestfulController extends Controller {
    * @param type $model
    */
   private function search($model) {
+    if (!in_array($model, $this->getSearchableModels())) {
+      $this->_sendResponse(500, 'Error: REST transation are not supported for ' . $model);
+    }
     // Get the respective model instance
     $json = file_get_contents('php://input');
     $put_vars = CJSON::decode($json, true);  
@@ -111,7 +130,24 @@ class RestfulController extends Controller {
       foreach ($put_vars as $var => $value) {
         // Does the model have this attribute? If not raise an error
         if ($search->hasAttribute($var)) {
-          $criteria->compare($var,$value); 
+          $value = urldecode($value);
+          // default operator is equality:
+          $operator = '=';
+          if (strpos($value, '< ') > -1) {
+            $operator = '<';
+          } else if (strpos($value, '<= ') > -1) {
+            $operator = '<=';
+          } else if (strpos($value, '> ') > -1) {
+            $operator = '>=';
+          } else if (strpos($value, '>= ') > -1) {
+            $operator = '>=';
+          } else if (strpos($value, '!= ') > -1) {
+            $operator = '!=';
+          }
+          if ($operator != '=') {
+            $value = substr($value, strpos($value, ' ') + 1);
+          }
+          $criteria->addCondition($var . $operator . '\'' . $value . '\'');
         }
         else
           $this->_sendResponse(500, sprintf('Parameter \'%s\' is not allowed for model \'%s\' - json was \'%s\'', $var, $model, $json));
@@ -126,7 +162,7 @@ class RestfulController extends Controller {
     // Did we get some results?
     if (empty($models)) {
       // No
-      $this->_sendResponse(200, sprintf('No items where found for model \'%s\'', $model));
+      $this->_sendResponse(400, sprintf('No items where found for model \'%s\'', $model));
     } else {
       // Prepare response
       $rows = array();
@@ -144,6 +180,9 @@ class RestfulController extends Controller {
    * @param type $model
    */
   private function view($id, $model) {
+    if (!in_array($model, $this->getViewableModels())) {
+      $this->_sendResponse(500, 'Error: REST transation are not supported for ' . $model);
+    }
     // Check if id was submitted via GET
     if (!isset($id)) {
       $this->_sendResponse(500, 'Error: Parameter id is missing');
@@ -178,6 +217,9 @@ class RestfulController extends Controller {
    * @param model $model
    */
   private function create($model) {
+    if (!in_array($model, $this->getCreatableModels())) {
+      $this->_sendResponse(500, 'Error: REST transation are not supported for ' . $model);
+    }
     $json = file_get_contents('php://input'); //$GLOBALS['HTTP_RAW_POST_DATA'] is not preferred: http://www.php.net/manual/en/ini.core.php#ini.always-populate-raw-post-data
     $put_vars = CJSON::decode($json, true);  //true means use associative array
     // Try to assign POST values to attributes
@@ -223,6 +265,9 @@ class RestfulController extends Controller {
    * @param type $model
    */
   private function update($id, $model) {
+    if (!in_array($model, $this->getUpdatableModels())) {
+      $this->_sendResponse(500, 'Error: REST transation are not supported for ' . $model);
+    }
     // Parse the PUT parameters. This didn't work: parse_str(file_get_contents('php://input'), $put_vars);
 //    parse_str(file_get_contents('php://input'), $put_vars);
     $json = file_get_contents('php://input'); //$GLOBALS['HTTP_RAW_POST_DATA'] is not preferred: http://www.php.net/manual/en/ini.core.php#ini.always-populate-raw-post-data
@@ -295,7 +340,7 @@ class RestfulController extends Controller {
    * @param string $body
    * @param type $content_type
    */
-  private function _sendResponse($status = 200, $body = '', $content_type = 'text/html') {
+  protected function _sendResponse($status = 200, $body = '', $content_type = 'text/html') {
     // set the status
     $status_header = 'HTTP/1.1 ' . $status . ' ' . $this->_getStatusCodeMessage($status);
     header($status_header);
@@ -360,7 +405,7 @@ class RestfulController extends Controller {
    * @param type $status
    * @return type
    */
-  private function _getStatusCodeMessage($status) {
+  protected function _getStatusCodeMessage($status) {
     // these could be stored in a .ini file and loaded
     // via parse_ini_file()... however, this will suffice
     // for an example
@@ -380,22 +425,29 @@ class RestfulController extends Controller {
   /**
    * 
    */
-  private function _checkAuth() {
+  protected function _checkAuth() {
+    if (Yii::app()->params['esb_rest_api_on'] != 'true') {
+      $this->_sendResponse(500, 'Error: There is no REST API: ' . Yii::app()->params['esb_rest_api_on']);
+    }
     // Check if we have the USERNAME and PASSWORD HTTP headers set? 
-    if (!(isset($_SERVER['HTTP_X_' . self::APPLICATION_ID . '_USERNAME']) and isset($_SERVER['HTTP_X_' . self::APPLICATION_ID . '_PASSWORD']))) {
+    if (!(isset($_SERVER['HTTP_X_' . $this->getApplicationId() . '_USERNAME']) and isset($_SERVER['HTTP_X_' . $this->getApplicationId() . '_PASSWORD']))) {
       // Error: Unauthorized 
       $this->_sendResponse(401);
     }
-    $username = $_SERVER['HTTP_X_' . self::APPLICATION_ID . '_USERNAME'];
-    $password = $_SERVER['HTTP_X_' . self::APPLICATION_ID . '_PASSWORD'];
+    $username = $_SERVER['HTTP_X_' . $this->getApplicationId() . '_USERNAME'];
+    $password = $_SERVER['HTTP_X_' . $this->getApplicationId() . '_PASSWORD'];
+    
+    if (!in_array($username, Yii::app()->params['esb_rest_api_users'])) {
+      $this->_sendResponse(401, 'Error: Permissions.');
+    }
     // Find the user 
     $user = User::model()->find('LOWER(username)=?', array(strtolower($username)));
     if ($user === null) {
       // Error: Unauthorized 
-      $this->_sendResponse(401, 'Error: User Name is invalid');
+      $this->_sendResponse(401, 'Error: Invalid login parameters.');
     } else if (!$user->validatePassword($password)) {
       // Error: Unauthorized 
-      $this->_sendResponse(401, 'Error: User Password is invalid');
+      $this->_sendResponse(401, 'Error: Invalid login parameters.');
     }
   }
 
