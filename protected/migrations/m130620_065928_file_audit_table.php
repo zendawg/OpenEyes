32e811bf55e3bfb3ad722e47e21a64a59d549acd
zendawg <rich@statefive.org>
2013-06-21 07:56:47 +0100

    Removed index/fk that was causing trouble in audits.

--- protected/migrations/m130620_065928_file_audit_table.php
+++ protected/migrations/m130620_065928_file_audit_table.php
@@ -16,15 +16,6 @@ class m130620_065928_file_audit_table extends OEMigration {
                 'dest_parent' => 'int(10) unsigned default NULL',
                 'src_child' => 'int(10) unsigned default NULL',
                 'dest_child' => 'int(10) unsigned default NULL'), $this->getDefaults($tableName)), 'ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin');
-    
-        $this->addForeignKey('fs_file_src_parent_id_fk', 'fs_file_audit', 'src_parent', 'fs_directory', 'id');
-        $this->createIndex('src_parent', 'fs_file_audit', 'src_parent');
-        $this->addForeignKey('fs_file_src_child_id_fk', 'fs_file_audit', 'src_child', 'fs_file', 'id');
-        $this->createIndex('src_child', 'fs_file_audit', 'src_child');
-        $this->addForeignKey('fs_file_dest_parent_id_fk', 'fs_file_audit', 'dest_parent', 'fs_directory', 'id');
-        $this->createIndex('dest_parent', 'fs_file_audit', 'dest_parent');
-        $this->addForeignKey('fs_file_dest_child_id_fk', 'fs_file_audit', 'dest_child', 'fs_file', 'id');
-        $this->createIndex('dest_child', 'fs_file_audit', 'dest_child');
   }
 
   public function safeDown() {
