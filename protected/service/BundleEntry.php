f340710c98738253d0499d2fb7e74d0b1fee3287
Ben Spencer <ben.spencer@openeyes.org.uk>
2014-01-23 13:26:02 +0000

    Bundle data object.

---
+++ protected/service/BundleEntry.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * (C) OpenEyes Foundation, 2014
+ * This file is part of OpenEyes.
+ * OpenEyes is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * OpenEyes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with OpenEyes in a file titled COPYING. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package OpenEyes
+ * @link http://www.openeyes.org.uk
+ * @author OpenEyes <info@openeyes.org.uk>
+ * @copyright Copyright (C) 2014, OpenEyes Foundation
+ * @license http://www.gnu.org/licenses/gpl-3.0.html The GNU General Public License V3.0
+ */
+
+namespace Service;
+
+class BundleEntry extends DataObject
+{
+	/**
+	 * @param Resource $resource
+	 * @return BundleEntry
+	 */
+	static public function fromResource(Resource $resource)
+	{
+		return new self(
+			array(
+				'title' => get_class($resource),  // TODO Something more sensible
+				'id' => \Yii::app()->fhirMap->getUrl($resource->getReference()),
+				'updated' => date('c'),
+				'resource' => $resource,
+			)
+		);
+	}
+
+	public $title;
+	public $id;
+	public $updated;
+	public $resource;
+}
